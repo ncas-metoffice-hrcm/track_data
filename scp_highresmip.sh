df7e2d62819d8c68ef21a9d3b541c95bf8367242
Alex Baker <alexander.baker@reading.ac.uk>
2026-08-03 17:09:47 +0100

    Add files via upload

--- scp_highresmip.sh
+++ scp_highresmip.sh
@@ -20,6 +20,12 @@ elif [ "${hemisphere}" == "SH" ]; then
     sign="neg"
 fi
 
+if [[ "${inst}" == EC* ]]; then
+    grid="gr"
+else
+    grid="gn"
+fi
+
 for y in $(seq 1950 2014); do
     echo $y
     if [ "${hemisphere}" == "NH" ]; then
@@ -27,7 +33,7 @@ for y in $(seq 1950 2014); do
     elif [ "${hemisphere}" == "SH" ]; then
         season="jul-jun${y}$((y+1))"
     fi
-    scp -r ${username}@xfer-vm-02.jasmin.ac.uk:~/huracan/data/tracks/tropical_cyclones/TRACK/HiResMIP/HiResMIP/${inst}/${model}/${forcing}/TC/${hemisphere}/${model}_${forcing}_${variant}_gn_VOR_vertavg_${season}_T63/tr_trs_${sign}.2day_addvorT63_addwind_addmslp.tcident.new /Users/bakera/Downloads/
+    scp -r ${username}@xfer-vm-02.jasmin.ac.uk:~/huracan/data/tracks/tropical_cyclones/TRACK/HiResMIP/HiResMIP/${inst}/${model}/${forcing}/TC/${hemisphere}/${model}_${forcing}_${variant}_${grid}_VOR_vertavg_${season}_T63/tr_trs_${sign}.2day_addvorT63_addwind_addmslp.tcident.new /Users/bakera/Downloads/
     echo ${out_path}/tr_trs_${sign}.${y}.2day_addvorT63_addwind_addmslp.tcident.new
     mv ~/Downloads/tr_trs_${sign}.2day_addvorT63_addwind_addmslp.tcident.new ${out_path}/tr_trs_${sign}.${season}.2day_addvorT63_addwind_addmslp.tcident.new
 done
